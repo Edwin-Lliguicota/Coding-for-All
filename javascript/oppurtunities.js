341b0c867d9a3f3e303e45f33f27479c5308cedf
Edwin-Lliguicota <edwin.lliguicota062@gmail.com>
2022-08-18 19:26:40 -0400

    new
    hurr

--- javascript/oppurtunities.js
+++ javascript/oppurtunities.js
@@ -24,3 +24,29 @@ window.onclick = function(event) {
   }
 }
 
+
+// Get the modal
+var modalOne = document.getElementById("myModal-one");
+
+// Get the button that opens the modal
+var btnOne = document.getElementById("myBtn-one");
+
+// Get the <span> element that closes the modal
+var spanOne = document.getElementsByClassName("close-one")[0];
+
+// When the user clicks on the button, open the modal
+btnOne.onclick = function() {
+  modalOne.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+spanOne.onclick = function() {
+  modalOne.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalOne) {
+    modalOne.style.display = "none";
+  }
+}
